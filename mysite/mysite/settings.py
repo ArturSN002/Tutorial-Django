113005c7ced60321cb00e066ea262f43f2513716
ArturSN002 <asn0373@gmail.com>
2021-12-23 11:36:57 -0300

    segundo commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,12 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTF - 8'
 
 USE_I18N = True
 
--- mysite/polls/apps.py
+++ mysite/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AplConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apl'
+    name = 'polls'
